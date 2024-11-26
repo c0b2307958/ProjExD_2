98b8a0528b1912ee5615a3c6bf46c6e0c5ce529d
c0b23079 <c0b2307958@edu.teu.ac.jp>
2024-11-26 14:29:33 +0900

    練習2

--- ex2/dodge_bomb.py
+++ ex2/dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -19,6 +20,12 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0,HEIGHT)
+    vx,vy = +5, +5
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
@@ -36,6 +43,8 @@ def main():
                 sum_mv[1]+=tpl[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
